da9fc2ec26342854ee4ea415c22f30233d961bbb
ronyhassansam03n <ronyhassansam03n@gmail.com>
2022-12-10 23:07:06 +0600

    all done

--- src/Header/Exprinces/Exprinces.js
+++ src/Header/Exprinces/Exprinces.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Exprinces = () => {
     return (
         <div>
-            this is exprincess
+            <h1 className='text-5xl'>Experience</h1> <br></br>
+            <h1 className='text-3xl'>RS Boiler Ltd.</h1>
+            <h1 className='text-2xl'>2021 - Present</h1> <br></br>
+            <p className='text-xl'> Sales & marketing Executive
+                RS Boiler is one of the top repudiated Steam Boiler Manufacturer Company in Bangladesh. I worked there 1.5 years as a Sales & Marketing Executive. Because of my Position in this Company, I am so fortunate to work with Top Rated Companies in Bangladesh. Such as, Bombay Sweets Ltd, Global Leaf Bangladesh, CP Food, Akon Group, AJ Group, Central Jail in Dhaka, Apex fashion wear Ltd, Bengal leather , Ripon Group and many more.</p>
         </div>
     );
 };
