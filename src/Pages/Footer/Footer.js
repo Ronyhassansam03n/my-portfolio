01ec7f53bad5390c6d00c80fcf1894757e2f4cd7
ronyhassansam03n <ronyhassansam03n@gmail.com>
2022-12-10 21:52:55 +0600

    footer

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Pic from './../../assets/Rony Hassan.png'
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
     return (
         <div>
-            this is Footer.
+            <footer className="footer p-10 bg-neutral text-neutral-content mt-10">
+                <div>
+                    <Link to='/'><img className='w-24' src={Pic} alt=''></img> </Link>
+                    <p>MD RONY HASSAN<br />Web Developer.</p>
+                </div>
+                <div>
+                    <span className="footer-title">Social</span>
+                    <div className="grid grid-flow-col gap-4">
+                        <a href='https://github.com/Ronyhassansam03n'> <FaGithubSquare></FaGithubSquare></a>
+                        <a href='https://www.linkedin.com/in/md-rony-hassan-15bb11214/'> <FaLinkedin></FaLinkedin></a>
+                    </div>
+                </div>
+            </footer>
         </div>
     );
 };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Backpic from './../../assets/back3.png'
 
 const Home = () => {
     return (
-        <div className='mx-16 mt-10'>
-            <h1 className='text-5xl font-semibold'> Hi,</h1>
-            <h1 className='text-5xl font-semibold'> I'm Rony,</h1>
-            <h1 className='text-5xl font-semibold'> Web  <span className='text-5xl font-semibold text-primary-focus'>Developer.</span></h1>
+        <div className='mx-16 mt-10 flex gap-20'>
 
-            <div className='mt-10 '>
-                <Link to='/' className="btn btn-sm mx-2  btn-outline btn-primary font-bold text-md ">See Resume</Link>
-                <Link to='/contact' className="btn btn-sm btn-outline btn-primary font-bold text-md">Contact Me</Link>
+            <div>
+                <h1 className='text-5xl font-semibold'> Hi,</h1>
+                <h1 className='text-5xl font-semibold'> I'm Rony,</h1>
+                <h1 className='text-5xl font-semibold'> Web  <span className='text-5xl font-semibold text-primary-focus'>Developer.</span></h1>
+
+                <div className='mt-10 '>
+                    <a className="btn btn-sm mx-2  btn-outline btn-primary font-bold text-md " href='https://drive.google.com/file/d/1Y5uG9CM_1dc2rsDGqoklbiimRtgjBdu-/view?usp=sharing'>See Resume</a>
+                    <Link to='/contact' className="btn btn-sm btn-outline btn-primary font-bold text-md">Contact Me</Link>
+                </div>
+
             </div>
+            <div>
+
+                <img src={Backpic} alt=''></img>
+            </div>
+
 
         </div>
 
