fba14b69f16928257722f8d240b85f8fa11d71ab
ronyhassansam03n <ronyhassansam03n@gmail.com>
2022-12-10 19:56:36 +0600

    header done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div>
-            this is home
+        <div className='mx-16 mt-10'>
+            <h1 className='text-5xl font-semibold'> Hi,</h1>
+            <h1 className='text-5xl font-semibold'> I'm Rony,</h1>
+            <h1 className='text-5xl font-semibold'> Web  <span className='text-5xl font-semibold text-primary-focus'>Developer.</span></h1>
+
+            <div className='mt-10 '>
+                <Link to='/' className="btn btn-sm mx-2  btn-outline btn-primary font-bold text-md ">See Resume</Link>
+                <Link to='/contact' className="btn btn-sm btn-outline btn-primary font-bold text-md">Contact Me</Link>
+            </div>
+
         </div>
+
+
     );
 };
 
--- src/Pages/Navber/Navber.js
+++ src/Pages/Navber/Navber.js
@@ -10,17 +10,17 @@ const Navber = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 font-bold">
                         <li><Link to='/'>Home</Link></li>
                         <li><Link to='/projects'>Projects</Link></li>
                         <li><Link to='/experience'>Experience</Link></li>
 
                     </ul>
                 </div>
-                <img className='w-50' src={Pic} alt=''></img>
+                <Link to='/'><img className='w-50' src={Pic} alt=''></img> </Link>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1 gap-5">
+                <ul className="menu menu-horizontal px-1 gap-5 font-bold text-md">
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to='/projects'>Projects</Link></li>
                     <li><Link to='/experience'>Experience</Link></li>
@@ -28,7 +28,7 @@ const Navber = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <Link to='/contact' className="btn btn-outline">Contact Me</Link>
+                <Link to='/contact' className="btn btn-outline btn-primary font-bold text-md">Contact Me</Link>
             </div>
         </div>
     );
