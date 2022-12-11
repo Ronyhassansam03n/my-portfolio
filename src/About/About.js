a03d0b0c6df2fe5facf23ca797c6ae3c6dea87a7
ronyhassansam03n <ronyhassansam03n@gmail.com>
2022-12-11 22:23:19 +0600

    ready to all in fly

--- src/About/About.js
+++ src/About/About.js
@@ -3,7 +3,24 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1 className='text-5xl font-bold text-primary text-center mt-10'> Coooooming  Soon </h1>
+            <h1 className='text-3xl font-semibold mt-10'> Name : MD RONY HASSAN. </h1>
+            <h1 className='text-2xl font-semibold mt-1'>  Full Stack Web <span className='text-2xl font-semibold text-primary'>Developer</span> </h1>
+            <h1 className='text-2xl font-semibold mt-1'>  Years : <span className='text-2xl font-semibold text-primary'>26</span>  Years. </h1>
+            <h1 className='text-2xl font-semibold mt-1'>  Address : Uttara, Dhaka-<span className='text-2xl font-semibold text-primary'>1230</span>, Bangladesh. </h1>
+            <h1 className='text-2xl font-semibold mt-1'>  Nationality : Bangladeshi.</h1>
+            <h1 className='text-4xl font-semibold mt-5'>  Some Hobbies</h1>
+            <div className='divider w-96'></div>
+            <ul className='mt-5'>
+                <li><span className='text-primary'>#</span> PLAYING BILLIARD POOL </li>
+                <li><span className='text-primary'>#</span> SWIMMING </li>
+                <li><span className='text-primary'>#</span> TRAVELING</li>
+                <li><span className='text-primary'>#</span> PC GAMING</li>
+                <li><span className='text-primary'>#</span> CREATIVE THINKING</li>
+                <li><span className='text-primary'>#</span> PLAYING FOOTBALL </li>
+
+            </ul>
+
+
         </div>
     );
 };
