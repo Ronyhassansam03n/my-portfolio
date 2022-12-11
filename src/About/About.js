b179840d43a1bcc664ec59bd502ca43f9c8fb315
ronyhassansam03n <ronyhassansam03n@gmail.com>
2022-12-11 22:08:23 +0600

    ready to fly

--- src/About/About.js
+++ src/About/About.js
@@ -3,8 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            avout me
-
+            <h1 className='text-5xl font-bold text-primary text-center mt-10'> Coooooming  Soon </h1>
         </div>
     );
 };
